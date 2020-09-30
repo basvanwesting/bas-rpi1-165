ac62eb76d82a8e590ab1b21dccd135621df39c04
basvanwesting <basvanwesting@hotmail.com>
2020-09-30 16:02:36 +0200

    add projections for elixir use explcit celsius for TemperatureSensor

--- test/grove/temperature_sensor_test.exs
+++ test/grove/temperature_sensor_test.exs
@@ -2,10 +2,10 @@ defmodule Grove.TemperatureSensorTest do
   use ExUnit.Case
   alias Grove.TemperatureSensor
 
-  doctest TemperatureSensor
+  #doctest TemperatureSensor
 
-  test "calculate_temperature/1" do
-    temperature = TemperatureSensor.calculate_temperature(475)
+  test "calculate_celsius/1" do
+    temperature = TemperatureSensor.calculate_celsius(475)
     assert_in_delta(temperature, 22.05, 0.01)
   end
 end
